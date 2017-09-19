50e0581bcac6b35624ec9caa572871d81891b467
Mystery00 <mystery0dyl520@gmail.com>
2017-09-19 12:16:13 +0800

    日志上传

--- app/src/main/java/pw/janyo/whatanime/APP.java
+++ app/src/main/java/pw/janyo/whatanime/APP.java
@@ -1,11 +1,18 @@
 package pw.janyo.whatanime;
 
 import android.app.Application;
+import android.widget.Toast;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
+import pw.janyo.whatanime.classes.Response;
 import vip.mystery0.tools.CrashHandler.CatchExceptionListener;
 import vip.mystery0.tools.CrashHandler.CrashHandler;
+import vip.mystery0.tools.HTTPok.HTTPok;
+import vip.mystery0.tools.HTTPok.HTTPokResponse;
+import vip.mystery0.tools.HTTPok.HTTPokResponseListener;
 import vip.mystery0.tools.Logs.Logs;
 
 /**
@@ -14,6 +21,8 @@ import vip.mystery0.tools.Logs.Logs;
 
 public class APP extends Application
 {
+	private static final String TAG = "APP";
+
 	@Override
 	public void onCreate()
 	{
@@ -25,6 +34,44 @@ public class APP extends Application
 					@Override
 					public void onException(String date, File file, String appVersionName, int appVersionCode, String androidVersion, int sdk, String vendor, String model, Throwable throwable)
 					{
+						Map<String, Object> map = new HashMap<>();
+						map.put("logFile", file);
+						map.put("date", date);
+						map.put("appName", getString(R.string.app_name));
+						map.put("appVersionName", appVersionName);
+						map.put("appVersionCode", appVersionCode);
+						map.put("androidVersion", androidVersion);
+						map.put("sdk", sdk);
+						map.put("vendor", vendor);
+						map.put("model", model);
+						new HTTPok()
+								.setURL("http://123.206.186.70/php/uploadLog/upload_file.php")
+								.setRequestMethod(HTTPok.Companion.getPOST())
+								.setParams(map)
+								.isFileRequest()
+								.setListener(new HTTPokResponseListener()
+								{
+									@Override
+									public void onError(String s)
+									{
+										Logs.e(TAG, "onError: " + s);
+										Toast.makeText(getApplicationContext(), "日志上传失败！", Toast.LENGTH_SHORT)
+												.show();
+									}
+
+									@Override
+									public void onResponse(HTTPokResponse httPokResponse)
+									{
+										Response response = httPokResponse.getJSON(Response.class);
+										if (response.code == 0)
+											Toast.makeText(getApplicationContext(), "日志上传成功！", Toast.LENGTH_SHORT)
+													.show();
+										else
+											Toast.makeText(getApplicationContext(), "日志上传失败！", Toast.LENGTH_SHORT)
+													.show();
+									}
+								})
+								.open();
 
 					}
 				})
---
+++ app/src/main/java/pw/janyo/whatanime/classes/Response.java
@@ -0,0 +1,11 @@
+package pw.janyo.whatanime.classes;
+
+/**
+ * Created by myste.
+ */
+
+public class Response
+{
+	public int code;
+	public String message;
+}
