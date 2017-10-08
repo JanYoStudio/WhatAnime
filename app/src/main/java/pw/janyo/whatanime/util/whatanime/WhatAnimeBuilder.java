e2edd839709559347834953338d89ca8da8cccfc
Mystery00 <mystery0dyl520@gmail.com>
2017-10-08 13:07:36 +0800

    更新ide

--- app/src/main/java/pw/janyo/whatanime/util/whatanime/WhatAnimeBuilder.java
+++ app/src/main/java/pw/janyo/whatanime/util/whatanime/WhatAnimeBuilder.java
@@ -12,23 +12,18 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import pw.janyo.whatanime.classes.Animation;
-import pw.janyo.whatanime.classes.Dock;
 import pw.janyo.whatanime.classes.History;
 import pw.janyo.whatanime.listener.WhatAnimeBuildListener;
-import pw.janyo.whatanime.util.Settings;
 import pw.janyo.whatanime.util.WAFileUti;
 import vip.mystery0.tools.HTTPok.HTTPok;
 import vip.mystery0.tools.HTTPok.HTTPokException;
 import vip.mystery0.tools.HTTPok.HTTPokResponse;
 import vip.mystery0.tools.HTTPok.HTTPokResponseListener;
-import vip.mystery0.tools.Logs.Logs;
 
 /**
  * Created by myste.
@@ -36,7 +31,6 @@ import vip.mystery0.tools.Logs.Logs;
 
 public class WhatAnimeBuilder
 {
-	private static final String TAG = "WhatAnimeBuilder";
 	private WhatAnime whatAnime;
 	private OkHttpClient mOkHttpClient;
 	private History history;
@@ -85,13 +79,11 @@ public class WhatAnimeBuilder
 							String date = Calendar.getInstance().getTime().toString();
 							md.update(date.getBytes());
 							md5 = new BigInteger(1, md.digest()).toString(16);
-							Logs.i(TAG, "onResponse: " + md5);
-							File jsonFile = new File(context.getCacheDir() + File.separator + "json" + File.separator + md5);
+							File jsonFile = new File(context.getFilesDir() + File.separator + "json" + File.separator + md5);
 							jsonFile.getParentFile().mkdirs();
-							Logs.i(TAG, "onResponse: " + httPokResponse.getFile(jsonFile));
 							FileReader fileReader = new FileReader(jsonFile);
 							Animation animation = new Gson().fromJson(fileReader, Animation.class);
-							String cacheImgPath = context.getCacheDir() + File.separator + "img" + File.separator + md5;
+							String cacheImgPath = context.getFilesDir() + File.separator + "img" + File.separator + md5;
 							WAFileUti.fileCopy(history.getImaPath(), cacheImgPath);
 							history.setCachePath(cacheImgPath);
 							history.setTitle(animation.docs.get(0).title);
