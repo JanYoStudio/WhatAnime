b835c2b52a209567dc8cdae83b1285f968b83261
Mystery00 <mystery0dyl520@gmail.com>
2018-01-22 17:53:04 +0800

    去掉SpotsDialog，更新依赖库，准备修改源码

--- app/src/main/java/pw/janyo/whatanime/activity/ErrorActivity.java
+++ app/src/main/java/pw/janyo/whatanime/activity/ErrorActivity.java
@@ -25,95 +25,84 @@ import vip.mystery0.tools.Logs.Logs;
  * Created by myste.
  */
 
-public class ErrorActivity extends AppCompatActivity
-{
-	private static final String TAG = "ErrorActivity";
+public class ErrorActivity extends AppCompatActivity {
+    private static final String TAG = "ErrorActivity";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_error);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
 
-		TextView text_date = findViewById(R.id.text_date);
-		TextView text_version = findViewById(R.id.text_version);
-		TextView text_SDK = findViewById(R.id.text_SDK);
-		TextView text_vendor = findViewById(R.id.text_vendor);
-		TextView text_model = findViewById(R.id.text_model);
-		TextView text_exception = findViewById(R.id.text_exception);
-		Button button = findViewById(R.id.button_upload);
+        TextView text_date = findViewById(R.id.text_date);
+        TextView text_version = findViewById(R.id.text_version);
+        TextView text_SDK = findViewById(R.id.text_SDK);
+        TextView text_vendor = findViewById(R.id.text_vendor);
+        TextView text_model = findViewById(R.id.text_model);
+        TextView text_exception = findViewById(R.id.text_exception);
+        Button button = findViewById(R.id.button_upload);
 
-		final SpotsDialog spotsDialog = new SpotsDialog(this, getString(R.string.hint_upload_log), R.style.SpotsDialog);
-		final UploadHandler uploadHandler = new UploadHandler();
-		uploadHandler.spotsDialog = spotsDialog;
-		uploadHandler.activity = this;
-		uploadHandler.coordinatorLayout = findViewById(R.id.coordinatorLayout);
+        final SpotsDialog spotsDialog = new SpotsDialog(this, getString(R.string.hint_upload_log), R.style.SpotsDialog);
+        final UploadHandler uploadHandler = new UploadHandler();
+        uploadHandler.spotsDialog = spotsDialog;
+        uploadHandler.activity = this;
+        uploadHandler.coordinatorLayout = findViewById(R.id.coordinatorLayout);
 
-		if (getIntent().getBundleExtra("error") == null)
-			finish();
-		final Error error = (Error) getIntent().getBundleExtra("error").getSerializable("error");
-		if (error != null)
-		{
-			text_date.setText(getString(R.string.exception_time, error.time));
-			text_version.setText(getString(R.string.exception_version, error.appVersionName, error.appVersionCode));
-			text_SDK.setText(getString(R.string.exception_sdk, error.AndroidVersion, error.sdk));
-			text_vendor.setText(getString(R.string.exception_vendor, error.vendor));
-			text_model.setText(getString(R.string.exception_model, error.model));
-			StringWriter stringWriter = new StringWriter();
-			error.throwable.printStackTrace(new PrintWriter(stringWriter));
-			text_exception.setText(getString(R.string.exception_message, stringWriter.toString()));
-			button.setOnClickListener(new View.OnClickListener()
-			{
-				@Override
-				public void onClick(View view)
-				{
-					spotsDialog.show();
-					Map<String, Object> map = new HashMap<>();
-					map.put("logFile", getIntent().getBundleExtra("error").getSerializable("file"));
-					map.put("date", error.time);
-					map.put("appName", getString(R.string.app_name));
-					map.put("appVersionName", error.appVersionName);
-					map.put("appVersionCode", error.appVersionCode);
-					map.put("androidVersion", error.AndroidVersion);
-					map.put("sdk", error.sdk);
-					map.put("vendor", error.vendor);
-					map.put("model", error.model);
-					new HTTPok()
-							.setURL("http://202.5.19.14/interface/uploadLog.php")
-							.setRequestMethod(HTTPok.Companion.getPOST())
-							.setParams(map)
-							.isFileRequest()
-							.setListener(new HTTPokResponseListener()
-							{
-								@Override
-								public void onError(String s)
-								{
-									Logs.e(TAG, "onError: " + s);
-									uploadHandler.sendEmptyMessage(-1);
-								}
+        if (getIntent().getBundleExtra("error") == null)
+            finish();
+        final Error error = (Error) getIntent().getBundleExtra("error").getSerializable("error");
+        if (error != null) {
+            text_date.setText(getString(R.string.exception_time, error.time));
+            text_version.setText(getString(R.string.exception_version, error.appVersionName, error.appVersionCode));
+            text_SDK.setText(getString(R.string.exception_sdk, error.AndroidVersion, error.sdk));
+            text_vendor.setText(getString(R.string.exception_vendor, error.vendor));
+            text_model.setText(getString(R.string.exception_model, error.model));
+            StringWriter stringWriter = new StringWriter();
+            error.throwable.printStackTrace(new PrintWriter(stringWriter));
+            text_exception.setText(getString(R.string.exception_message, stringWriter.toString()));
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    spotsDialog.show();
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("logFile", getIntent().getBundleExtra("error").getSerializable("file"));
+                    map.put("date", error.time);
+                    map.put("appName", getString(R.string.app_name));
+                    map.put("appVersionName", error.appVersionName);
+                    map.put("appVersionCode", error.appVersionCode);
+                    map.put("androidVersion", error.AndroidVersion);
+                    map.put("sdk", error.sdk);
+                    map.put("vendor", error.vendor);
+                    map.put("model", error.model);
+                    new HTTPok()
+                            .setURL("http://202.5.19.14/interface/uploadLog.php")
+                            .setRequestMethod(HTTPok.Companion.getPOST())
+                            .setParams(map)
+                            .isFileRequest()
+                            .setListener(new HTTPokResponseListener() {
+                                @Override
+                                public void onError(String s) {
+                                    Logs.e(TAG, "onError: " + s);
+                                    uploadHandler.sendEmptyMessage(-1);
+                                }
 
-								@Override
-								public void onResponse(HTTPokResponse httPokResponse)
-								{
-									try
-									{
-										Response response = httPokResponse.getJSON(Response.class);
-										if (response.code == 0)
-										{
-											uploadHandler.response = response;
-											uploadHandler.sendEmptyMessage(0);
-										} else
-											uploadHandler.sendEmptyMessage(-1);
-									} catch (Exception e)
-									{
-										e.printStackTrace();
-										uploadHandler.sendEmptyMessage(-1);
-									}
-								}
-							})
-							.open();
-				}
-			});
-		}
-	}
+                                @Override
+                                public void onResponse(HTTPokResponse httPokResponse) {
+                                    try {
+                                        Response response = httPokResponse.getJSON(Response.class);
+                                        if (response.code == 0) {
+                                            uploadHandler.response = response;
+                                            uploadHandler.sendEmptyMessage(0);
+                                        } else
+                                            uploadHandler.sendEmptyMessage(-1);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                        uploadHandler.sendEmptyMessage(-1);
+                                    }
+                                }
+                            })
+                            .open();
+                }
+            });
+        }
+    }
 }
