798f2a001a24292e012c3fb600efddcba4a96828
Mystery0 <mystery0dyl520@gmail.com>
2020-01-21 14:47:20 +0800

    Revert "fix: 添加对启动页请求的时长限制"
    This reverts commit 16c4635c

--- app/src/main/java/pw/janyo/whatanime/module/NetworkModule.kt
+++ app/src/main/java/pw/janyo/whatanime/module/NetworkModule.kt
@@ -11,45 +11,30 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 val networkModule = module {
-	single(named("must4OkHttpClient")) {
-		OkHttpClient.Builder()
-				.connectTimeout(4, TimeUnit.SECONDS)
-				.build()
-	}
-	single(named("normalOkHttpClient")) {
+	single {
 		OkHttpClient.Builder()
 				.connectTimeout(40, TimeUnit.SECONDS)
 				.readTimeout(40, TimeUnit.SECONDS)
 				.build()
 	}
-	single(named("baseRetrofit")) {
+	single(named("base")) {
 		Retrofit.Builder()
 				.baseUrl(Constant.baseUrl)
-				.client(get(named("normalOkHttpClient")))
+				.client(get())
 				.addConverterFactory(GsonConverterFactory.create())
 				.build()
-	}
-	single(named("must4Retrofit")) {
-		Retrofit.Builder()
-				.baseUrl(Constant.cloudUrl)
-				.client(get(named("must4OkHttpClient")))
-				.addConverterFactory(GsonConverterFactory.create())
-				.build()
-	}
-	single(named("cloudRetrofit")) {
-		Retrofit.Builder()
-				.baseUrl(Constant.cloudUrl)
-				.client(get(named("normalOkHttpClient")))
-				.addConverterFactory(GsonConverterFactory.create())
-				.build()
-	}
-	single(named("base")) {
-		get<Retrofit>(named("baseRetrofit")).create(SearchApi::class.java)
 	}
 	single(named("cloud")) {
-		get<Retrofit>(named("cloudRetrofit")).create(ServerApi::class.java)
+		Retrofit.Builder()
+				.baseUrl(Constant.cloudUrl)
+				.client(get())
+				.addConverterFactory(GsonConverterFactory.create())
+				.build()
 	}
-	single(named("must4")) {
-		get<Retrofit>(named("must4Retrofit")).create(ServerApi::class.java)
+	single {
+		get<Retrofit>(named("base")).create(SearchApi::class.java)
+	}
+	single {
+		get<Retrofit>(named("cloud")).create(ServerApi::class.java)
 	}
 }
--- app/src/main/java/pw/janyo/whatanime/module/ViewModelModule.kt
+++ app/src/main/java/pw/janyo/whatanime/module/ViewModelModule.kt
@@ -1,7 +1,6 @@
 package pw.janyo.whatanime.module
 
 import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import pw.janyo.whatanime.viewModel.HistoryViewModel
 import pw.janyo.whatanime.viewModel.MainViewModel
@@ -9,7 +8,7 @@ import pw.janyo.whatanime.viewModel.TestViewModel
 
 val viewModelModule = module {
 	viewModel {
-		TestViewModel(get(named("must4")))
+		TestViewModel(get())
 	}
 	viewModel {
 		MainViewModel(get(), get())
