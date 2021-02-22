f90ae929ef8e26f1ec261bb59d6d580fc7db2939
Mystery0 <mystery0dyl520@gmail.com>
2021-02-22 23:30:56 +0800

    fix: 修复上传文件失败

--- app/src/main/java/pw/janyo/whatanime/api/SearchApi.kt
+++ app/src/main/java/pw/janyo/whatanime/api/SearchApi.kt
@@ -3,10 +3,12 @@ package pw.janyo.whatanime.api
 import okhttp3.RequestBody
 import pw.janyo.whatanime.model.Animation
 import pw.janyo.whatanime.model.SearchQuota
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface SearchApi {
-    @FormUrlEncoded
     @POST("/api/search")
     suspend fun search(@Body body: RequestBody): Animation
 
