543373b778825f8a76b8db466663ccae14b5534c
Mystery0 <mystery0dyl520@gmail.com>
2021-02-23 13:41:01 +0800

    fix: 移除luban，修复打包错误

--- app/src/main/java/pw/janyo/whatanime/module/appModule.kt
+++ app/src/main/java/pw/janyo/whatanime/module/appModule.kt
@@ -4,10 +4,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
-import top.zibin.luban.Luban
 
 val appModule = module {
 	single { androidContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager }
-
-	single { Luban.with(androidContext()) }
 }
