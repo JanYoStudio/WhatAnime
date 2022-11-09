0aa4ca8c8c9177847c6271bcd9daa8e6bbc618af
mystery0 <mystery0dyl520@gmail.com>
2022-11-09 23:21:12 +0800

    refactor: 适配Material You

--- app/src/main/java/pw/janyo/whatanime/ui/theme/Theme.kt
+++ app/src/main/java/pw/janyo/whatanime/ui/theme/Theme.kt
@@ -1,12 +1,16 @@
 package pw.janyo.whatanime.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import pw.janyo.whatanime.config.Configure
 import pw.janyo.whatanime.model.entity.NightMode
@@ -18,7 +22,6 @@ private val LightColorPalette = lightColorScheme()
 @Composable
 fun isDarkMode(): Boolean {
     val mode by Theme.nightMode.collectAsState()
-//    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
     return when (mode) {
         NightMode.AUTO -> isSystemInDarkTheme()
         NightMode.ON -> true
@@ -31,8 +34,14 @@ fun isDarkMode(): Boolean {
 fun WhatAnimeTheme(
     content: @Composable() () -> Unit
 ) {
-    val colorScheme = if (isDarkMode()) DarkColorPalette else LightColorPalette
-//    val colorScheme = LightColorPalette
+    val isDark = isDarkMode()
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = if (dynamicColor) {
+        val context = LocalContext.current
+        if (isDark) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    } else {
+        if (isDarkMode()) DarkColorPalette else LightColorPalette
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
