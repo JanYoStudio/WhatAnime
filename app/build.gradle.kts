34af867f83f370443924359e624abaec0a45a701
mystery0 <mystery0dyl520@gmail.com>
2022-11-12 12:55:25 +0800

    release: 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = gitVersionCode
-        versionName = "1.6.3"
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
