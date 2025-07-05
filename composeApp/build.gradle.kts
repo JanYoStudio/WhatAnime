f450754e8cf1f937513808e2fde8bc1f5580e2bf
Mystery0 <mystery0dyl520@gmail.com>
2025-07-05 17:27:18 +0800

    ci skip: 更新iOS构建流程和应用图标
    此提交引入了用于更新iOS应用版本信息的Gradle任务，并更改了应用图标文件格式。 同时，更新了Xcode项目配置，移除了动态设置的团队ID和产品包名，并更新了项目版本号。

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -50,8 +50,6 @@ kotlin {
             baseName = "ComposeApp"
             isStatic = true
             linkerOpts.add("-lsqlite3")
-            binaryOption("bundleVersion", gitVersionCode.toString())
-            binaryOption("bundleShortVersionString", appVersionName)
         }
     }
 
@@ -208,6 +206,22 @@ aboutLibraries {
     }
     export {
         outputFile = file("src/commonMain/composeResources/files/aboutlibraries.json")
+    }
+}
+
+tasks.register("updateAppleBuildVersion") {
+    doLast {
+        val iosAppVersionName = "${appVersionName}.r${gitVersionCode}.${gitVersionName}"
+        val config = rootProject.file("iosApp/Configuration/Config.xcconfig")
+        val content = config.readText()
+        var newContent =
+            content.replace(Regex("MARKETING_VERSION=.*"), "MARKETING_VERSION=${iosAppVersionName}")
+        newContent = newContent.replace(
+            Regex("CURRENT_PROJECT_VERSION=\\d+"),
+            "CURRENT_PROJECT_VERSION=${gitVersionCode}"
+        )
+        config.writeText(newContent)
+        println("Updated Config.xcconfig with version $iosAppVersionName (Build $gitVersionCode)")
     }
 }
 
